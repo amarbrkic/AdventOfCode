e65825ce82fa403197a70703c220e0166f15bf91
amarbrkic <amar.brkic202@gmail.com>
2021-11-20 23:11:29 +0100

    -

--- 2020/3/1.Toboggan Trajectory.py
+++ 2020/3/1.Toboggan Trajectory.py
@@ -1,3 +1,14 @@
+# NOT DONE
 with open("input.txt", "r") as f:
-    for line in f.readlines():
-        print(1)
+    pos = 0
+    br = 0
+    lines = f.readlines()
+    for i in range(len(lines)-1):
+        try:
+            if(lines[i+1][pos+3] == "#"):
+                br += 1
+        except:
+            if((lines[i+1]*(i/5))[pos+3] == "#"):
+                br += 1
+        pos += 3
+    print(br)
