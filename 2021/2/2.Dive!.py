c6c05febef3a8bb8e24e28d1a574ef746f157068
amarbrkic <amar.brkic202@gmail.com>
2021-12-02 14:21:05 +0100

    .

--- 2021/2/2.Dive!.py
+++ 2021/2/2.Dive!.py
@@ -1,4 +1,16 @@
 with open("input.txt", "r") as f:
     lines = f.readlines()
+    xPos = 0
+    yPos = 0
+    aim = 0
     for line in lines:
-        
+        direction = line.split()[0]
+        steps = int(line.split()[1])
+        if direction == "forward":
+            xPos += steps
+            yPos += steps*aim
+        elif direction == "down":
+            aim += steps
+        elif direction == "up":
+            aim -= steps
+    print(xPos*yPos)
